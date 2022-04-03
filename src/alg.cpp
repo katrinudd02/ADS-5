8131375b7b43a5c9be9ba761ef2dacdfe3189e1a
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-04-03 20:10:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,6 +28,20 @@ bool is_digit(char c) {
     return c >= '0' && c <= '9';
 }
 
+int calculator(char symbol, int a, int b) {
+    if (symbol == '+') {
+        return (a + b);
+    } else if (symbol == '-') {
+        return (b - a);
+    } else if (sembol == '*') {
+        return (a * b);
+    } else if ((symbol == '/') && (a != 0)) {
+        return (b / a);
+    } else {
+        return 0;
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
     stack <char> S;
     std::string out;
@@ -90,10 +104,20 @@ std::string infx2pstfx(std::string inf) {
 int eval(std::string out) {
     int result = 0;
     TStack <int, 100> resultStack;
-    int i;
+    int i = 0;
+    int a = 0;
+    int b = 0;
     for (; i < out.size(); i++) {
         if (prioritet(out[i]) == -1) {
             resultStack.push(out[i] - '0');
-    
-    return 0;
+        } else if (prioritet(out[i]) < 4) {
+            a = resultStack.get();
+            resultStack.pop();
+            b = resultStack.get();
+            resultStack.pop();
+            resultStack.push(calculator(out[k], a, b));
+        }
+    }
+    result = resultStack.get();    
+    return result;
 }

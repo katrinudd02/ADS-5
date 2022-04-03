4503a2c36012f3dcb4ac797cda1c065f38fb7038
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-04-04 01:53:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,68 +4,89 @@
 #include "tstack.h"
 
 int prioritet(char v) {
+    int res = -1;
     switch (v) {
-        case '(': return 0;
-        case ')': return 1;
-        case '+': return 2;
-        case '-': return 2;
-        case '*': return 3;
-        case '/': return 3;
+    case '(': res = 0;
+        break;
+    case ')': res = 1;
+        break;
+    case '+': res = 2;
+        break;
+    case '-': res = 2;
+        break;
+    case '*': res = 3;
+        break;
+    case '/': res = 3;
+        break;
     }
-    return -1;
+    return res;
 }
 
-bool is_digit(std::string pref) {
-    for (size_t i = 0; i < pref.size(); ++i) {
-        if (pref[i] < '0' || pref[i] > '9')
-            return false;
+bool is_digit(char s) {
+    bool res = false;
+    if (s >= '0' && s <= '9') {
+        res = true;
     }
-    return true;
+    return res;
 }
 
-int calculator(char symbol, int a, int b) {
-    switch (symbol) {
-        case '*': return a * b;
-        case '/': return a / b;
-        case '-': return a - b;
-        case '+': return a + b;
-        default: return -1;
+int calculator(char v, int a, int b) {
+    int res = 0;
+    switch (v) {
+    case '*': res = a * b;
+        break;
+    case '/': res = a / b;
+        break;
+    case '-': res = a - b;
+        break;
+    case '+': res = a + b;
+        break;
     }
+    return res;
 }
 
 std::string infx2pstfx(std::string inf) {
     TStack <char, 100> S;
     std::string out;
     int i = 0, j = 0;
+    int pr = 0;
     for (; inf[i] != '\0'; i++) {
-        int pr = prioritet(inf[i]);
-        if (pr == -1) {
-            if (!out.empty() && prioritet(inf[i - 1]) != -1) {
+        pr = prioritet(inf[i]);
+        if (is_digit(inf[i])) {
+            out.push_back(inf[i]);
+        }
+        else if (pr != -1) {
+            if (out.length() != 0 && is_digit(out.back())) {
                 out.push_back(' ');
             }
-            out.push_back(inf[i]);
-        } else if (pr == 0 || pr > prioritet(S.get()) || S.isEmpty()) {
-            out.push_back(inf[i]);
-        } else {
             if (pr == 1) {
-                while (S.get() != '(') {
-                    out.push_back(' ');
+                while (S.get() != '(' && !S.isEmpty()) {
                     out.push_back(S.get());
                     S.pop();
                 }
-                S.pop();
-            } else {
-                while (prioritet(S.get()) >= pr) {
-                    out.push_back(' ');
-                    out.push_back(S.get());
+                if (S.get() == '(') {
                     S.pop();
                 }
+            }
+            else if (pr == 0 || pr > prioritet(S.get()) || S.isEmpty()) {
                 S.push(inf[i]);
+
+            }
+            else {
+                while (!S.isEmpty() && prioritet(S.get()) >= pr) {
+                    out.push_back(S.get());
+                    S.pop();
+                }
+                if (pr == 2 || pr == 3) {
+                    S.push(inf[i]);
+                }
             }
         }
     }
-    while (!S.isEmpty()) {
+    if (inf.length() != 0 && is_digit(inf[inf.length() - 1])) {
         out.push_back(' ');
+    }
+    while (!S.isEmpty()) {
         out.push_back(S.get());
         S.pop();
     }
@@ -78,17 +99,24 @@ int eval(std::string out) {
     int i = 0;
     int a = 0;
     int b = 0;
-    while (i < out.length()) {
-        if (prioritet(out[i]) == -1) {
-            if (out[i] != ' ') {
-                S.push(out[i] - '0');
-            }
-        } else if (prioritet(out[i]) < 4) {
-            a = S.get();
-            S.pop();
+    int dig = 0;
+    int len = out.length();
+    while (i < len) {
+        if (prioritet(out[i]) == 2 || prioritet(out[i]) == 3) {
             b = S.get();
             S.pop();
-            S.push(calculator(out[i], b, a));
+            a = S.get();
+            S.pop();
+            S.push(calculator(out[i], a, b));
+        }
+        else if (out[i] == ' ') {
+            S.push(dig);
+            dig = 0;
+        }
+        else {
+            if (is_digit(out[i])) {
+                dig = dig * 10 + (int)out[i] - 48;
+            }
         }
         i++;
     }
